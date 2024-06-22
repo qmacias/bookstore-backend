3fb8babeaf81c278a2ed257bfebf63d76492c69c
qmacias <ezequielqmacias@gmail.com>
2024-06-22 14:53:43 -0300

    Add Initial Files

---
+++ README.md
@@ -0,0 +1,18 @@
+# Getting Started
+
+We're using Python v3.10.0 and Flask v3.0.3
+
+## Bookstore routes
+
+Here the client interacts with the provided Flask API endpoints.
+
+| Method | Url                                | Description  |
+|--------|------------------------------------|--------------|
+| GET    | http://127.0.0.1:5000/health_check | Health check |
+
+---
+
+Health check
+```bash
+http http://127.0.0.1:5000/health_check
+```
---
+++ api/index.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, make_response, Response
+
+app = Flask(__name__)
+
+
+@app.after_request
+def after_request_func(response):
+    origin = request.headers.get('Origin')
+    if request.method == 'OPTIONS':
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Headers', 'x-csrf-token')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+        if origin:
+            response.headers.add('Access-Control-Allow-Origin', origin)
+    else:
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        if origin:
+            response.headers.add('Access-Control-Allow-Origin', origin)
+
+    return response
+
+
+@app.route('/health_check', methods=['GET'])
+def health_check():
+    return Response(
+        '', 200, {
+            'Content-Type': 'application/json', 'Location': f'{request.url_rule.rule}'
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run(load_dotenv=True)
